045c5e308672523f4ee8a48cceefa321ffcbcad0
kuaikuai <yuxiaofeng3@YUXIAOFENG3.Hisense.ad>
2016-03-30 14:23:22 +0800

    ok

---
+++ org/set_rps.sh
@@ -0,0 +1,19 @@
+
+#!/bin/bash
+# Enable RPS (Receive Packet Steering)
+
+rfc=4096
+cc=$(grep -c processor /proc/cpuinfo)
+rsfe=$(echo $cc*$rfc | bc)
+sysctl -w net.core.rps_sock_flow_entries=$rsfe
+for fileRps in $(ls /sys/class/net/eth*/queues/rx-*/rps_cpus)
+do
+    echo ffffffff > $fileRps
+done
+
+for fileRfc in $(ls /sys/class/net/eth*/queues/rx-*/rps_flow_cnt)
+do
+    echo $rfc > $fileRfc
+done
+
+tail /sys/class/net/eth*/queues/rx-*/{rps_cpus,rps_flow_cnt}
